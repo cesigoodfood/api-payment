ff0f96565c803fc4d62213bbbe99dd2e613850a5
AlexandreDAUSSIN <alexandre.daussin@gmail.com>
2026-02-04 16:30:46 +0100

    Update du payment

--- GoodFood.Payment/Program.cs
+++ GoodFood.Payment/Program.cs
@@ -17,7 +17,9 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.Host("localhost", "/", h =>
+        var rabbitHost = builder.Configuration["RabbitMq:Host"] ?? "localhost";
+
+        cfg.Host(rabbitHost, "/", h =>
         {
             h.Username("guest");
             h.Password("guest");
